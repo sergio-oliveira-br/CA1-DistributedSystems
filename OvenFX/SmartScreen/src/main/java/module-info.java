7ee6ffb058a4c275e6c5324e577cc932830c7590
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-04-13 22:57:31 +0100

    created a new project but thats wrong
    attempt to create my JavaFX

---
+++ OvenFX/SmartScreen/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.ca.smartscreen {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.ca.smartscreen to javafx.fxml;
+    exports com.ca.smartscreen;
+}
