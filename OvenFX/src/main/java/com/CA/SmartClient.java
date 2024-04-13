b7679a2a8745e162c2203033c0e600447f8ec065
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-04-13 13:05:11 +0100

    Implemented Client Side
    Client Side has constructor and method

--- OvenFX/src/main/java/com/CA/SmartClient.java
+++ OvenFX/src/main/java/com/CA/SmartClient.java
@@ -15,9 +15,74 @@
 
 package com.CA;
 
+import com.CA.gRPC.GreeterGrpc;
+import com.CA.gRPC.HelloReply;
+import com.CA.gRPC.HelloRequest;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 public class SmartClient
 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    /*
+        ===========================
+            Instance Variables
+        ===========================
+     */
+
+    private final ManagedChannel channel;
+    private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
+
+    /*
+        ===========================
+            Instantiating a stub
+        ===========================
+     */
+
+    //Constructor: Create a gRPC channel for our stub, specifying the port we want to connect
+    public SmartClient(String host, int port)
+    {
+        this(ManagedChannelBuilder.forAddress(host, port).usePlaintext());
+    }
+
+    //Constructor: Accessing SmartServer server using the existing channel.
+    public SmartClient(ManagedChannelBuilder<?> channelBuilder)
+    {
+        channel = channelBuilder.build();
+
+        //blocking/synchronous stub:
+        //this means that the RPC call waits for the server to respond, and will either return a response or raise an exception
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    /*
+        ===========================
+            Implement Method
+        ===========================
+     */
+
+    // Client-side logic for interacting with the gRPC service.
+    public void greet(String name)
+    {
+        // Creating a request with the user's name.
+        HelloRequest request = HelloRequest.newBuilder().setName(name).build();
+
+        //Create a local variable and call the method
+        HelloReply response;
+        response = blockingStub.sayHello(request);
+
+        System.out.println("Hi " + response.getMessage());
+    }
+
+
+
+    public static void main(String[] args)
+    {
+
+
+
+
+
+
     }
 }
