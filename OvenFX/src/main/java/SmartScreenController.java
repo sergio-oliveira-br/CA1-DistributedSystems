326c12a81ece9fd3415b4b449cf352236296133c
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-04-14 11:47:51 +0100

    learning to create the functionality
    learning to create and how the system javaFX works

--- OvenFX/src/main/java/SmartScreenController.java
+++ OvenFX/src/main/java/SmartScreenController.java
@@ -2,26 +2,60 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextInputDialog;
+
+import java.util.Optional;
 
 public class SmartScreenController
 {
 
-    @FXML
-    private Button button;
 
     @FXML
-    private void handleButtonAction(ActionEvent event)
+    private Button sayHelloButton;
+
+    @FXML
+    private void sayHelloAction(ActionEvent event)
     {
-        System.out.println("Button clicked! kkkk");
+        System.out.println("Hello Sergio");
+    }
+
+
+
+
+    @FXML
+    private Button setTempButton;
+
+    @FXML
+    private void submitTempAction(ActionEvent event)
+    {
+
+        //Create a dialog box to request user input
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Input Dialog");
+        dialog.setHeaderText("Please enter a number:");
+        dialog.setContentText("Number:");
+
+        //Display the dialog box and wait for user input
+        Optional<String> result = dialog.showAndWait();
+
+        //Process user input
+        result.ifPresent(number ->
+        {
+            try
+            {
+                int intValue = Integer.parseInt(number);
+                System.out.println("You entered: " + intValue);
+                
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+            }
+        });
+
 
     }
 
-    @FXML
-    private CheckBox checkB;
 
-    @FXML
-    public void myCheck(ActionEvent event)
-    {
-        System.out.println("This is test");
-    }
+
+
 }
